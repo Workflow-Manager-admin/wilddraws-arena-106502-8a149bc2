4d705345e5fd671b5ced66b442ce38fafaa1a68f
Kavia AI <production@kavia.ai>
2025-07-04 11:08:25 +0000

    CheckPoint

--- frontend_web_app/src/components/index.js
+++ frontend_web_app/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as LoginScreen } from "./LoginScreen";
 export { default as MainGameScreen } from "./MainGameScreen";
+
+// Convenience re-export in case indirect import is used elsewhere
